aee9d1ade6d3c6946160ad19fc3b126e6141b050
Jonathan Damico <jj_damico@yahoo.com>
2016-06-27 17:02:49 -0700

    init commit for uber's side of things

--- js.js
+++ js.js
@@ -4,4 +4,21 @@ $(document).ready(function () {
     console.log(location.coords.longitude);
     console.log(location.coords.accuracy);
   });
+  $.ajax({
+    url: 'https://api.uber.com/v1/estimates/price?start_latitude=34.146056&start_longitude=-118.746635&end_latitude=34.048117&end_longitude=-118.5060167&seat_count=1',
+    dataType: 'json',
+    success: function(output) {
+      for (var price in prices) {
+        $('#'+price.display_name).append('<th>'+price.localized_display_name+'</th><th>'+(price.low_estimate+price.high_estimate)/2*price.surge_multiplier+'</th>');
+      }
+    },
+    method: "GET",
+    headers: {
+      "authorization": "Token v_KOeq4LzWvcEhWFHQIUvHCUWWJN5OiSS46ckv5p",
+      "cache-control": "no-cache",
+      "postman-token": "2a7156f2-93a9-c089-597d-d6aeeb622b2b"
+    },
+    async: false,
+    timeout: 5000,
+  });
 });
