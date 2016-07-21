169fc4d5e5241f93d9a6416c2314013ea9dd3d45
Jonathan Damico <jj_damico@yahoo.com>
2016-07-21 10:39:14 -0700

    pauvre lyft

--- js.js
+++ js.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
     dataType: 'json',
     success: function(output) {
       for (var i = 0; i < output.cost_estimates.length; i++) {
-        estimatedCost = (output.cost_estimates[i].estimated_cost_cents_min+output.prices[i].estimated_cost_cents_max)/2;
+        estimatedCost = (output.cost_estimates[i].estimated_cost_cents_min+output.cost_estimates[i].estimated_cost_cents_max)/2;
         tablehtml+='<tr><td>'+output.cost_estimates[i].display_name+'</td><td>'+estimatedCost%100+'.'+parseInt(estimatedCost/100)+'</td></tr>';
       }
     },
