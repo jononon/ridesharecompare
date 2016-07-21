1ba4e2adf2f453cd750624b6a19fb3a36f4457a8
Jonathan Damico <jj_damico@yahoo.com>
2016-07-21 15:32:54 -0700

    way better code, sorting of rides based on price

--- js.js
+++ js.js
@@ -8,13 +8,42 @@ $(document).ready(function () {
   startLng = -118.748221;
   endLat = 34.0481852;
   endLng = -118.5038797;
-  tablehtml = "<tr><th>Ride</th><th>Cost</th><th>Go!</th></tr>";
+  var rides = [];
   $.ajax({
     url: 'https://api.uber.com/v1/estimates/price?start_latitude='+startLat+'&start_longitude='+startLng+'&end_latitude='+endLat+'&end_longitude='+endLng+'&seat_count=1',
     dataType: 'json',
     success: function(output) {
       for (var i = 0; i < output.prices.length; i++) {
-        tablehtml+='<tr><td>'+output.prices[i].localized_display_name+'</td><td>'+output.prices[i].estimate+'</td><td><a href=https://m.uber.com/ul?action=setPickup&pickup[latitude]='+startLat+'&pickup[longitude]='+startLng+'&pickup[nickname]=inVia&pickup[formatted_address]=3304%20Derry%20Ave%2C%20Agoura%20Hills%2C%20CA%2091301&dropoff[latitude]='+endLat+'&dropoff[longitude]='+endLng+'&dropoff[formatted_address]=1124%20Napoli%20Drive%2C%20Pacific%20Palisades%2C%20CA%2090272&product_id='+output.prices[i].product_id+'><button type="button" class="btn btn-default">Request</button></a></td></tr>';
+        rides.push(
+          {
+            name: output.prices[i].localized_display_name,
+            highEstimate: output.prices[i].high_estimate,
+            lowEstimate: output.prices[i].low_estimate,
+            estimate: output.prices[i].estimate,
+            service: "Uber",
+            product_id: output.prices[i].product_id,
+            orderLink: 'https://m.uber.com/ul?action=setPickup&pickup[latitude]='+startLat+'&pickup[longitude]='+startLng+'&pickup[nickname]=inVia&pickup[formatted_address]=3304%20Derry%20Ave%2C%20Agoura%20Hills%2C%20CA%2091301&dropoff[latitude]='+endLat+'&dropoff[longitude]='+endLng+'&dropoff[formatted_address]=1124%20Napoli%20Drive%2C%20Pacific%20Palisades%2C%20CA%2090272&product_id='+output.prices[i].product_id,
+            eta: undefined
+          }
+        );
+      }
+    },
+    method: "GET",
+    headers: {
+      "authorization": "Token v_KOeq4LzWvcEhWFHQIUvHCUWWJN5OiSS46ckv5p",
+    },
+    async: false,
+    timeout: 5000,
+  });
+  $.ajax({
+    url: 'https://api.uber.com/v1/estimates/time?start_latitude='+startLat+'&start_longitude='+startLng,
+    dataType: 'json',
+    success: function(output) {
+      for (var i = 0; i < output.times.length; i++) {
+        for (var j = 0; j < rides.length; j++) {
+          if(rides[j].product_id == output.times[i].product_id)
+            rides[j].eta = ''+output.times[i].estimate%60+':'+parseInt(output.times[i].estimate/60);
+        }
       }
     },
     method: "GET",
@@ -29,8 +58,19 @@ $(document).ready(function () {
     dataType: 'json',
     success: function(output) {
       for (var i = 0; i < output.cost_estimates.length; i++) {
-        estimatedCost = (output.cost_estimates[i].estimated_cost_cents_min+output.cost_estimates[i].estimated_cost_cents_max)/2;
-        tablehtml+='<tr><td>'+output.cost_estimates[i].display_name+'</td><td>$'+estimatedCost%100+'.'+parseInt(estimatedCost/100)+'</td><td><a href=lyft://ridetype?id='+output.cost_estimates[i].ride_type+'&pickup[latitude]='+startLat+'&pickup[longitude]='+startLng+'&destination[latitude]='+endLat+'&destination[longitude]='+endLng+'><button type="button" class="btn btn-default">Request</button></a></td></tr>';
+        estimatedCost = (output.cost_estimates[i].estimated_cost_cents_min+output.cost_estimates[i].estimated_cost_cents_max)/2/100;
+        rides.push(
+          {
+            name: output.cost_estimates[i].display_name,
+            highEstimate: output.cost_estimates[i].estimated_cost_cents_max/100,
+            lowEstimate: output.cost_estimates[i].estimated_cost_cents_min/100,
+            estimate: estimatedCost,
+            service: "Lyft",
+            product_id: output.cost_estimates[i].ride_type,
+            orderLink: 'lyft://ridetype?id='+output.cost_estimates[i].ride_type+'&pickup[latitude]='+startLat+'&pickup[longitude]='+startLng+'&destination[latitude]='+endLat+'&destination[longitude]='+endLng,
+            eta: undefined
+          }
+        );
       }
     },
     method: "GET",
@@ -39,8 +79,42 @@ $(document).ready(function () {
     },
     async: false,
     timeout: 5000,
-  })
-   $('#prices').append(tablehtml);
+  });
+  $.ajax({
+    url: 'https://api.lyft.com/v1/cost?lat='+startLat+'&lng='+startLng,
+    dataType: 'json',
+    success: function(output) {
+      for (var i = 0; i < output.eta_estimates.length; i++) {
+        for (var j = 0; j < rides.length; j++) {
+          if(rides[j].product_id == output.eta_estimates[i].ride_type)
+            rides[j].eta = ''+output.eta_estimates[i].eta_seconds%60+':'+parseInt(output.eta_estimates[i].eta_seconds/60);
+        }
+      }
+    },
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer gAAAAABXkQMyvsFHAKvOJcT9uTFDy_jHYnIHzuS3gU-Ad3_GbD7E18UDtLGB_tzKTMw2Z9Az1Q5-gjR8k8la973MXE3cfAN7M5xF-kk-kk0pygUPvmS4mo7i8LAcfBr3_oVRH6nWYv8wTMOVfdhMh8KBfFEvqtdEyPtOZNUxlPAC9ljvrxEY6bE_svIcVGHSWx-n0lPxJPwDS6DHSFIo4fB9fM5R5kVGcg==",
+    },
+    async: false,
+    timeout: 5000,
+  });
+
+  function comparePrice(a,b) {
+    comparisonPriceA = (a.highEstimate+a.lowEstimate)/2;
+    comparisonPriceB = (b.highEstimate+b.lowEstimate)/2;
+    if(comparisonPriceA < comparisonPriceB)
+      return -1;
+    else if(comparisonPriceA > comparisonPriceB)
+      return 1;
+    else
+      return 0;
+  }
+  rides.sort(comparePrice);
+  tablehtml = "<tr><th>Ride</th><th>Cost</th><th>Go!</th></tr>";
+  for (var i = 0; i < rides.length; i++) {
+    tablehtml+='<tr><td>'+rides[i].name+'</td><td>'+rides[i].estimate+'</td><td>'+rides[i].eta+'</td><td><a href='+rides[i].orderLink+'><button type="button" class="btn btn-default">Request</button></a></td></tr>'
+  }
+  $('#prices').append(tablehtml);
 });
 /*curl -X POST -H "Content-Type: application/json" \
      --user "v3OmgqE86Nhu:197pfsoEIr-I_wYYBMA7-sUaMB9zknAx" \
