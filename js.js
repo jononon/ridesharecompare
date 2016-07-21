b7c7db7ca88fa1ceb1e8ee3316cc4a0a7ac2e280
Jonathan Damico <jj_damico@yahoo.com>
2016-07-21 09:42:24 -0700

    add bootstrap and fix my stupidity

--- js.js
+++ js.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     url: 'https://api.uber.com/v1/estimates/price?start_latitude=34.146056&start_longitude=-118.746635&end_latitude=34.048117&end_longitude=-118.5060167&seat_count=1',
     dataType: 'json',
     success: function(output) {
-      for (var price in prices) {
+      for (var price in output.prices) {
         $('#'+price.display_name).append('<th>'+price.localized_display_name+'</th><th>'+(price.low_estimate+price.high_estimate)/2*price.surge_multiplier+'</th>');
         console.log(price);
       }
@@ -21,3 +21,26 @@ $(document).ready(function () {
     timeout: 5000,
   });
 });
+
+function initMap() {
+      var center = {lat: 51.5037819, lng: -0.2243051};
+      // Create a map object and specify the DOM element for display.
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+          style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+          mapTypeIds: [
+            google.maps.MapTypeId.ROADMAP,
+            google.maps.MapTypeId.HYBRID
+          ]
+        },
+        scrollwheel: true,
+        zoom: 13
+      });
+      // Create a marker and set its position.
+      var marker = new google.maps.Marker({
+        map: map,
+        position: {lat: 51.491541, lng: -0.176366}
+      });
+    }
