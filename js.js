d896d1354a9cea048e0b9b2a29df1e7613c1def6
Jonathan Damico <jj_damico@yahoo.com>
2016-07-21 17:11:50 -0700

    more formatting and dates!

--- js.js
+++ js.js
@@ -128,6 +128,7 @@ $(document).ready(function () {
     tablehtml+='<tr><td>'+rides[i].companyLogo+' '+rides[i].name+'</td><td>'+(rides[i].surge!=1?' '+rides[i].surgeText+' '+rides[i].surge:'')+'</td><td>'+rides[i].estimate+'</td><td>'+rides[i].eta+'</td><td><a href='+rides[i].orderLink+'><button type="button" class="btn btn-default">Request</button></a></td></tr>'
   }
   $('#prices').html(tablehtml);
+  $('#lastUpdated').html("Last Updated: "+(new Date()).toLocaleString());
 });
 /*curl -X POST -H "Content-Type: application/json" \
      --user "v3OmgqE86Nhu:197pfsoEIr-I_wYYBMA7-sUaMB9zknAx" \
