589cecfc295603746e02ac1ce90249a8fce1c9ee
Jonathan Damico <jj_damico@yahoo.com>
2016-08-22 22:42:47 -0400

    concat pls

--- js.js
+++ js.js
@@ -318,7 +318,7 @@ function calculateTransitTimes(start, end) {
         estimate:"",
         service: "Transit",
         product_id: "Transit",
-        orderLink: 'https://www.google.com/maps/dir/'+start.lat+','+start.lng+'/'+end.lat+','end.lng,
+        orderLink: 'https://www.google.com/maps/dir/'+start.lat+','+start.lng+'/'+end.lat+','+end.lng,
         eta: ((travelTime/60/60>=1)?""+parseInt(travelTime/60/60)+" hours and ":"")+travelTime/60%60+" minutes"
       });
     }
