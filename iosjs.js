7d421827a865e923ea19824637f73e2921c20284
Jonathan Damico <jj_damico@yahoo.com>
2017-07-07 15:50:28 -0700

    aisugf

--- iosjs.js
+++ iosjs.js
@@ -161,7 +161,7 @@ function calculateRides (startLat, startLng, endLat, endLng) {
       return 0;
   }
   rides.sort(comparePrice);
-  document.write(JSON.stringify(rides));
+  document.write(rides);
 
   console.log(JSON.stringify(rides))
 }
